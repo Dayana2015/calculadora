de3d90e396565330ddbe4be32073d99619a95787
Dayana Sanchez <mosanz02@gmail.com>
2023-03-17 21:51:06 -0500

    versión 1.2

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,5 @@
+
+var botones=["7","8","9","+","4","5","6","-","1","2","3","0",".","=","/","%"];
 function renderizarGUI(){
 
     //division calculadora
@@ -30,6 +32,42 @@ function renderizarGUI(){
     const divBotones= document.createElement("div")
     divBotones.setAttribute("id", "botones");
     divCalculadora.appendChild(divBotones);
+
+    for(let i=0;i<botones.length;i++){
+        //crear filas
+        if(i%4==0){
+            const divFila=document.createElement("div");
+            divFila.setAttribute("class", "row");
+            divBotones.appendChild(divFila);
+        }
+        let boton=document.createElement("button");
+        boton.setAttribute("id","boton" + botones[i]);
+        boton.setAttribute("class","btn btn-primary col-3 border-white");
+        boton.innerHTML= botones[i];//para agregar algo que contenga html
+        divBotones.lastChild.appendChild(boton);
+        //agregar un escuchador de eventos darle a boton la capacidad de escuchar lo evento del usuario
+        boton.addEventListener("click", function(){
+            procesarEvento(boton);
+        });
+        divBotones.lastChild.appendChild(boton);
+    }
+    function procesarEvento(boton){
+        let miPantalla=document.getElementById("pantalla");
+
+        if(miPantalla.value=="0"){
+            miPantalla.value="";
+
+        }
+        if(boton.innerHTML != "="){
+            miPantalla.value+=boton.innerHTML;
+        }else{
+            //evaluar la expresin con math.js
+            let resultado=math.evaluate(miPantalla.value);
+            miPantalla.value=resultado;
+
+        }
+        
+    }
 
 }
 renderizarGUI();
